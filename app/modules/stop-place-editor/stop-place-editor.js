4dbf66507db4fcc2663bc5e66cac69a6700c634d
Cristoffer Solem <cristoffer.solem@netlight.com>
2015-12-21 16:08:43 +0100

    POC-127 Make it possible to select quay markers and update form as well.

--- app/modules/stop-place-editor/stop-place-editor.js
+++ app/modules/stop-place-editor/stop-place-editor.js
@@ -12,13 +12,20 @@ angular.module('abzu.stopPlaceEditor', ['ngRoute'])
 .controller('StopPlaceEditorCtrl', ['$window', '$routeParams', '$scope', 'stopPlaceService', 'stopPlaceTypeService', 'leafletData', 'appConfig',
 	function($window, $routeParams, $scope, stopPlaceService, stopPlaceTypeService, leafletData, config) {
 
-
-		var extraMarkerIcon = {
+		var stopPlaceMarkerIcon = {
             type: 'extraMarker',
             icon: 'ion-gear-a',
-            markerColor: 'grey',
+            markerColor: 'red',
             prefix: 'ion',
             shape: 'circle' 
+		};
+
+		var quayMarkerIcon = {
+            type: 'extraMarker',
+            icon: 'ion-android-bus',
+            markerColor: 'grey',
+            shape: 'square',
+            prefix: 'ion',
 		};
 
         $scope.definedLayers = {
@@ -40,8 +47,11 @@ angular.module('abzu.stopPlaceEditor', ['ngRoute'])
                     osm: $scope.definedLayers.osm,
                     local_map: $scope.definedLayers.local_map
                 }
-            }
+            },
+            markers: {}
         });
+
+        $scope.currentQuay = {};
 
 	    $scope.toggleLayer = function(layerName) {
             var baselayers = $scope.layers.baselayers;
@@ -51,7 +61,6 @@ angular.module('abzu.stopPlaceEditor', ['ngRoute'])
                 baselayers[layerName] = $scope.definedLayers[layerName];
             }
 	    };
-
 
 		$scope.update = function() {
         	$scope.master = angular.copy($scope.stopPlace);
@@ -69,54 +78,122 @@ angular.module('abzu.stopPlaceEditor', ['ngRoute'])
 	    	$scope.stopPlace.quays.push({});
 	    };
 
+	    $scope.isEditingQuay = function(quayId) {
+	    	if(!$scope.currentQuay.id) return false;
+	    	return $scope.currentQuay.id == quayId;
+	    };
+
+	    $scope.quayFormClicked = function(quay) {
+			console.log("Quay form clicked for quay with id: "+quay.id);
+
+	    	var markerKey = createMarkerKey(quay.id);
+	    	$scope.markers[markerKey].focus = false;
+	    	$scope.markers[markerKey].focus = true;
+
+	    };
+
+	    var createBoundsArray = function(centroid) {
+	    	return [centroid.location.latitude, centroid.location.longitude];
+	    };
+
+	    var createQuayMarker = function(quay) {
+
+	    	var markerKey = createMarkerKey(quay.id);
+
+			$scope.markers[markerKey] = {
+				lat: quay.centroid.location.latitude,
+				lng: quay.centroid.location.longitude,
+				message: "Stoppunkt: "+quay.name,
+				icon: quayMarkerIcon,
+				focus: false,
+	            draggable: true,
+	            markerKey: markerKey,
+	            id: quay.id
+			}
+	    };
+
+	    var createQuayMarkers = function(quays, bounds) {
+			for(var i in quays) {
+				var quay = quays[i];
+				createQuayMarker(quay);
+				bounds.push(createBoundsArray(quay.centroid));
+			}
+	    };
+
+
+
+
 	    var populateStopPlace = function(stopPlace) {
+
+
+	    	console.log("Populate stop place");
 			$scope.stopPlace = stopPlace;
 			$scope.master = angular.copy($scope.stopPlace);
-
-			var latitude = $scope.stopPlace.centroid.location.latitude;
-			var longitude = $scope.stopPlace.centroid.location.longitude;
+	    	$scope.currentObject = $scope.stopPlace;
 
 	        var bounds = [];
-            bounds.push([latitude, longitude]);
+            bounds.push(createBoundsArray($scope.stopPlace.centroid));
 
+	    	var markerKey = createMarkerKey($scope.stopPlace.id);
 
-			
-
-			$scope.markers = {
-				mainMarker: {
-					lat: latitude,
-					lng: longitude,
-					message: $scope.stopPlace.name,
-					focus: true,
-		            draggable: true,
-		            icon: extraMarkerIcon
-				}			
-			};
-
-
+			$scope.markers[markerKey] = {
+				message: $scope.stopPlace.name,
+				focus: true,
+	            draggable: true,
+	            icon: stopPlaceMarkerIcon,
+	            lat: $scope.stopPlace.centroid.location.latitude,
+	            lng: $scope.stopPlace.centroid.location.longitude,
+	            markerKey: markerKey,
+	            id : $scope.stopPlace.id
+			};	
+		
 			if(stopPlace.quays) {
-				for(var i in stopPlace.quays) {
-					var quay = stopPlace.quays[i];
-					$scope.markers["quay1"] = {
-						lat: quay.centroid.location.latitude,
-						lng: quay.centroid.location.longitude,
-						message: "Stoppunkt "+quay.name,
-						icon: extraMarkerIcon,
-						focus: true,
-			            draggable: true
-					}
-
-					bounds.push([quay.centroid.location.latitude, quay.centroid.location.longitude]);
-
-				}
-				
+				createQuayMarkers(stopPlace.quays, bounds);
 			}
 
-			$scope.originalMarker = angular.copy($scope.markers);
 
             $scope.$on("leafletDirectiveMarker.dragend", function(event, args){
-                $scope.stopPlace.centroid.location.latitude = args.model.lat.toString();
-                $scope.stopPlace.centroid.location.longitude = args.model.lng.toString();
+            	console.log("marker dragged for object with id "+args.model.id);
+                $scope.currentObject.centroid.location.latitude = args.model.lat.toString();
+                $scope.currentObject.centroid.location.longitude = args.model.lng.toString();
+            });
+
+             $scope.$on("leafletDirectiveMarker.popupopen", function(event, args){
+            	console.log("Popup open on marker for object with id "+args.model.id);
+           
+            	if(args.model.id && args.model.id == $scope.stopPlace.id) {
+
+            		console.log("We are currently editing a stop place");
+
+            		$scope.currentQuay = {};
+            		$scope.currentObject = $scope.stopPlace;
+
+            		for(var m in $scope.markers) {
+            			if($scope.markers[m].id != $scope.stopPlace.id) {
+            				$scope.markers[m].focus = false;
+            			}
+            		}
+
+            	} else if(args.model.id) {
+
+
+            		console.log("We are currently not editing a stop place, but a quay. "+args.model.id);
+
+
+	    			for(var q in $scope.stopPlace.quays) {
+	    				var quay = $scope.stopPlace.quays[q];
+
+	    				if(quay.id == args.model.id) {
+	    					$scope.currentObject = quay;
+	    					$scope.currentQuay = quay;
+            				$scope.markers[createMarkerKey($scope.stopPlace.id)].focus = false;
+
+	    					break;
+	    				}
+	    			}
+            	}
+
+
             });
 
             leafletData.getMap().then(function(map) {
@@ -142,21 +219,25 @@ angular.module('abzu.stopPlaceEditor', ['ngRoute'])
 			return boundingBox;
         };
 
+        var createMarkerKey = function(id) {
+        	return id.replace(/[-]+/g, '_');
+        };
+
 	    var populateNearbyMarkers = function(stopPlacesWithinBoundingBox) {
 			console.log("Got " + stopPlacesWithinBoundingBox.length +" stop places from current bounding box");
 
-			$scope.markers = angular.copy($scope.originalMarker);
 	
 			for(var i in stopPlacesWithinBoundingBox) {
 
 				var relatedStopPlace = stopPlacesWithinBoundingBox[i];
 				
-				var key = relatedStopPlace.id.replace(/[-]+/g, '_');
 
 				if(relatedStopPlace.id == $scope.stopPlace.id) {
 					//console.log("Ignoring stop place with id " + $scope.stopPlace.id);
 					continue;
 				}
+
+				var key = createMarkerKey(relatedStopPlace.id);
 
 	            $scope.markers[key] = {
 	                lat: relatedStopPlace.centroid.location.latitude,
