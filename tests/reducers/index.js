a470bcd2ea7a523734871dc082da911bc75474a3
Sean Scully <sean.erik.scully@gmail.com>
2016-09-21 08:48:48 +0200

    Added unit tests for stopPlaces reducer

--- tests/reducers/index.js
+++ tests/reducers/index.js
@@ -8,23 +8,97 @@ import { stopPlacesReducer } from './../../reducers/'
 
 describe('stop places reducer', () => {
 
-  it('should return the initial state', () => {
+  it('Should return the initial state', () => {
     expect(stopPlacesReducer(null, {})).toNotEqual({})
   })
 
-  it('should return stop places', () => {
+  it('Should correctly set center position of stop places map', () => {
 
     const stopPlacesExample = JSON.parse(fs.readFileSync(__dirname + '/../creators/json/stopPlaces.json', 'utf-8'))
+    const centerPosition = [
+      59.918792999999994,
+      10.734455
+    ]
 
     expect(stopPlacesReducer({}, {
-        type: types.SUCCESS_STOP_NAMES,
-        payLoad: stopPlacesExample
+      type: types.CHANGED_MAP_CENTER,
+      payLoad: centerPosition
     }))
     .toEqual({
-        stopPlaceNames: {
-          places:
-            stopPlacesExample
-        }
+      centerPosition: centerPosition
     })
   })
-})
+
+  it('Should set an active marker used for stop places map', () => {
+    const formattedStopPlace = JSON.parse(fs.readFileSync(__dirname
+      + '/json/formattedStopPlace.json', 'utf-8'))
+
+      expect(stopPlacesReducer({}, {
+        type: types.SET_ACTIVE_MARKERS,
+        payLoad: formattedStopPlace
+      }))
+      .toEqual({
+        activeMarkers: [formattedStopPlace]
+      })
+    })
+
+    it('Should zoom level of stop places map', () => {
+
+      const zoomLevel = 13
+
+      expect(stopPlacesReducer({}, {
+        type: types.SET_ZOOM,
+        payLoad: zoomLevel
+      }))
+      .toEqual({
+        zoom: zoomLevel
+      })
+    })
+
+    it('Requesting stop names should indicate loading', () => {
+
+      const isLoading = true
+
+      expect(stopPlacesReducer({}, {
+        type: types.REQUESTED_STOP_NAMES
+      }))
+      .toEqual({
+        stopPlaceNames: {
+          isLoading: isLoading
+        }
+      })
+
+    })
+
+    it('Receiving stop names should return stop names', () => {
+
+      const stopPlaces = [JSON.parse(fs.readFileSync(__dirname
+        + '/json/formattedStopPlace.json', 'utf-8'))]
+
+        expect(stopPlacesReducer({}, {
+          type: types.RECEIVED_STOP_NAMES,
+          payLoad: stopPlaces
+        }))
+        .toEqual({
+          stopPlaceNames: {
+            places: stopPlaces
+          }
+        })
+
+      })
+
+      it('Should retturn error message upon running into an error when receiving stop names', () => {
+
+        const errorMessage = 'Error 404 – Not Found'
+
+        expect(stopPlacesReducer({}, {
+          type: types.ERROR_STOP_NAMES,
+          payLoad: errorMessage
+        }))
+        .toEqual({
+          stopPlaceNames: {
+            errorMessage: errorMessage
+          }
+        })
+      })
+    })
