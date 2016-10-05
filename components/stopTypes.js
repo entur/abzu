c786e23378b0d341c9fa8feadaec52ca794862ca
Sean Scully <sean.erik.scully@gmail.com>
2016-10-05 14:49:03 +0200

    added language support for stop types

--- components/stopTypes.js
+++ components/stopTypes.js
@@ -1,35 +1,74 @@
-const stopTypes = [{
-      name: "Busstopp",
-      value: "onstreetBus"
-    }, {
-      name: "Bytrikker",
-      value: "onstreetTram"
-    }, {
-      name: "Flyplass",
-      value: "airport"
-    }, {
-      name: "Togstopp",
-      value: "railStation"
-    }, {
-      name: "T-banestopp",
-      value: "metroStation"
-    }, {
-      name: "Alle typer buss",
-      value: "busStation"
-    }, {
-      name: "Havn",
-      value: "harbourPort"
-    }, {
-      name: "Fergekai",
-      value: "ferryStop"
-    }, {
-      name: "Kabelbane",
-      value: "liftStation"
-    },
+const stopTypes = {
+    "nb" : [
       {
-      name: "Annet",
-      value: "other"
-    }
-  ]
+        name: "Busstopp",
+        value: "onstreetBus"
+      }, {
+        name: "Bytrikker",
+        value: "onstreetTram"
+      }, {
+        name: "Flyplass",
+        value: "airport"
+      }, {
+        name: "Togstopp",
+        value: "railStation"
+      }, {
+        name: "T-banestopp",
+        value: "metroStation"
+      }, {
+        name: "Alle typer buss",
+        value: "busStation"
+      }, {
+        name: "Havn",
+        value: "harbourPort"
+      }, {
+        name: "Fergekai",
+        value: "ferryStop"
+      }, {
+        name: "Kabelbane",
+        value: "liftStation"
+      },
+        {
+        name: "Annet",
+        value: "other"
+      }
+    ],
+
+    "en" : [
+      {
+        name: "Bus stop",
+        value: "onstreetBus"
+      }, {
+        name: "City tram",
+        value: "onstreetTram"
+      }, {
+        name: "Airport",
+        value: "airport"
+      }, {
+        name: "Rail station",
+        value: "railStation"
+      }, {
+        name: "Metro stop",
+        value: "metroStation"
+      }, {
+        name: "Alle bus stops",
+        value: "busStation"
+      }, {
+        name: "Harbour port",
+        value: "harbourPort"
+      }, {
+        name: "Ferry stop",
+        value: "ferryStop"
+      }, {
+        name: "Lift station",
+        value: "liftStation"
+      },
+        {
+        name: "Other",
+        value: "other"
+      }
+    ]
+  }
+
 
   export default stopTypes
