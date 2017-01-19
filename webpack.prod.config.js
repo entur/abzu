a440afb0e77a9a0c0860b18cbdfebc8d80db7cba
Sean Scully <sean.erik.scully@gmail.com>
2017-01-19 14:32:41 +0100

    tries to solve png issue with jenkins

--- webpack.prod.config.js
+++ webpack.prod.config.js
@@ -52,8 +52,7 @@ module.exports = {
         test: /\.(jpe?g|png|gif|svg)$/i,
         exclude: /node_modules/,
         loaders: [
-          'file?hash=sha512&digest=hex&name=[hash].[ext]',
-          'image-webpack?bypassOnDebug&optimizationLevel=7&interlaced=false'
+          'file?hash=sha512&digest=hex&name=[hash].[ext]'
         ]
       }
     ]
