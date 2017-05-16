06e10579fc175823e52ca815cb77f97164649cda
Sean Scully <sean.erik.scully@gmail.com>
2017-05-16 12:03:34 +0200

    improve feedback for specific translation strings missing for test

--- test/languages/strings.spec.js
+++ test/languages/strings.spec.js
@@ -4,14 +4,24 @@ import en_lang from '../../static/lang/en.json'
 
 describe('language support', () => {
 
-  it('should support translations for all languages', () => {
+  it('should support translations for all strings in all languages', () => {
 
     const norwegianKeys = Object.keys(nb_lang).sort()
     const englishKeys = Object.keys(en_lang).sort()
 
+    let notInEnglish = norwegianKeys.filter( key => englishKeys.indexOf(key) === -1)
+    let notInNorwegian = englishKeys.filter( key => norwegianKeys.indexOf(key) === -1)
+
+    if (notInEnglish.length) {
+      console.info("Keys found in Norwegian not present in English: ", notInEnglish.join(','))
+    }
+
+    if (notInNorwegian.length) {
+      console.info("Keys found in English not present in Norwegian: ", notInNorwegian.join(','))
+    }
+
     expect(JSON.stringify(norwegianKeys)).toEqual(JSON.stringify(englishKeys))
 
   })
-
 
 })
