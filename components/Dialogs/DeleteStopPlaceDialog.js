2835e7afff81542e79082d669a1aa787f0d18f96
Sean Scully <sean.erik.scully@gmail.com>
2017-09-05 16:50:25 +0200

    NRP-1968 prevent user from deleting stop place if stop place is child of a parent stop place since this will cause constraint violation and unwanted side effects at the very best

--- components/Dialogs/DeleteStopPlaceDialog.js
+++ components/Dialogs/DeleteStopPlaceDialog.js
@@ -15,14 +15,17 @@ class DeleteStopPlaceDialog extends React.Component {
   };
 
   render() {
+
     const { open, intl, handleClose, handleConfirm, stopPlace } = this.props;
     const { formatMessage } = intl;
+    const { isChildOfParent } = stopPlace;
 
     const translations = {
       confirm: formatMessage({ id: 'confirm' }),
       cancel: formatMessage({ id: 'cancel' }),
       title: formatMessage({ id: 'delete_stop_title' }),
       info: formatMessage({ id: 'delete_stop_info' }),
+      cannotDelete: formatMessage({id: 'delete_stop_not_allowed'})
     };
 
     const actions = [
@@ -34,6 +37,7 @@ class DeleteStopPlaceDialog extends React.Component {
       <FlatButton
         label={translations.confirm}
         onTouchTap={handleConfirm}
+        disabled={!!isChildOfParent}
         primary={true}
         keyboardFocused={true}
         icon={<MdMerge />}
@@ -51,17 +55,27 @@ class DeleteStopPlaceDialog extends React.Component {
         }}
         contentStyle={{ width: '40%', minWidth: '40%', margin: 'auto' }}
       >
-        <div>
-          <div style={{ marginBottom: 10, color: '#000' }}>
-            <span style={{fontWeight: 600}}>{ `${stopPlace.name} (${stopPlace.id})` }</span>
-          </div>
-          <div style={{ marginLeft: 0, display: 'flex'}} >
-            <div style={{marginTop: 0, marginRight: 5}}>
-              <MdWarning color="orange"/>
+        {
+          !isChildOfParent ? (
+              <div>
+                <div style={{ marginBottom: 10, color: '#000' }}>
+                  <span style={{fontWeight: 600}}>{ `${stopPlace.name} (${stopPlace.id})` }</span>
+                </div>
+                <div style={{ marginLeft: 0, display: 'flex'}} >
+                  <div style={{marginTop: 0, marginRight: 5}}>
+                    <MdWarning color="orange"/>
+                  </div>
+                  <span>{translations.info}</span>
+                </div>
+              </div>
+            )
+          : <div style={{ marginLeft: 0, display: 'flex', alignItems: 'center'}}>
+              <div style={{marginTop: 0, marginRight: 5}}>
+                <MdWarning color="#dc2828" style={{width: 35, height: 35}}/>
+              </div>
+              <span>{translations.cannotDelete}</span>
             </div>
-            <span>{translations.info}</span>
-          </div>
-        </div>
+        }
       </Dialog>
     );
   }
