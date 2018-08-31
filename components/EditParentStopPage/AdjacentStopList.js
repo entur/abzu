cffd2be5dae15f3bf1aa7e43fa5a183184d8bbf0
Cristoffer Solem <cristoffer.solem@gmail.com>
2018-08-31 12:13:14 +0200

    ROR-483 Add component AdjacentStopList to be able to view the list of linked stops

---
+++ components/EditParentStopPage/AdjacentStopList.js
@@ -0,0 +1,56 @@
+/*
+ *  Licensed under the EUPL, Version 1.2 or – as soon they will be approved by
+the European Commission - subsequent versions of the EUPL (the "Licence");
+You may not use this work except in compliance with the Licence.
+You may obtain a copy of the Licence at:
+
+  https://joinup.ec.europa.eu/software/page/eupl
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the Licence is distributed on an "AS IS" basis,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the Licence for the specific language governing permissions and
+limitations under the Licence. */
+
+import { weightColors } from "../../models/weightTypes";
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { injectIntl } from 'react-intl';
+import EditorInsertLink from 'material-ui/svg-icons/editor/insert-link';
+
+class AdjacentStopList extends React.Component {
+
+  render() {
+
+    if(!this.props.stopPlace.adjacentSites) {
+      return null;
+    }
+
+    const refs = this.props.stopPlace.adjacentSites.map(adjacentRef => 
+      <div style={{fontSize: 13 }} key={adjacentRef}>
+      <EditorInsertLink
+              style={{transform: 'scale(0.6)' }}
+            />
+        {adjacentRef.ref}
+      </div>
+    );
+
+    console.log(refs)
+
+    return (
+      <div>
+        <span style={{fontWeight: 600, fontSize: '0.8em' }}>
+          This stop place is linked with adjancent stop places
+        </span>
+        {refs}
+      </div>
+    );
+  }
+}
+
+AdjacentStopList.propTypes = {
+  stopPlace: PropTypes.object.isRequired
+};
+
+export default injectIntl(AdjacentStopList);
+
