29e4284709627f8bc11b74927f5135c40e9c56a9
Tom Erik Støwer <testower@gmail.com>
2019-08-07 15:23:53 +0200

    Side-by-side theme providers

--- containers/App.js
+++ containers/App.js
@@ -13,29 +13,34 @@ See the Licence for the specific language governing permissions and
 limitations under the Licence. */
 
 import React from 'react';
-import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import getMuiTheme from 'material-ui/styles/getMuiTheme';
+import { MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles';
+import { MuiThemeProvider as V0MuiThemeProvider} from 'material-ui';
 import Header from '../components/Header';
 import { injectIntl } from 'react-intl';
 import { getTheme } from '../config/themeConfig';
 import SnackbarWrapper from '../components/SnackbarWrapper';
 import BrowserSupport from '../components/BrowserSupport';
 
+const muiThemeV0 = getMuiTheme(getTheme());
+const muiTheme = createMuiTheme(getTheme());
+
 class App extends React.Component {
 
   render() {
     const { children, intl } = this.props;
-    const muiTheme = getMuiTheme(getTheme());
 
     return (
-      <MuiThemeProvider muiTheme={muiTheme}>
-        <div>
-          <div className="version">v{process.env.VERSION}</div>
-          <Header intl={intl} />
-          {children}
-          <SnackbarWrapper formatMessage={intl.formatMessage}/>
-          <BrowserSupport/>
-        </div>
+      <MuiThemeProvider theme={muiTheme}>
+        <V0MuiThemeProvider muiTheme={muiThemeV0}>
+          <div>
+            <div className="version">v{process.env.VERSION}</div>
+            <Header intl={intl} />
+            {children}
+            <SnackbarWrapper formatMessage={intl.formatMessage}/>
+            <BrowserSupport/>
+          </div>
+        </V0MuiThemeProvider>
       </MuiThemeProvider>
     );
   }
