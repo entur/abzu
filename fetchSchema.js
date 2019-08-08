33ef4bf21a1315c8e82b9320d4cdc8fa44273cb3
Tom Erik Støwer <testower@gmail.com>
2019-08-08 08:55:37 +0200

    Make fetchSchema node v7.2.1 friendly1

--- fetchSchema.js
+++ fetchSchema.js
@@ -5,21 +5,16 @@ const graphqlFetch = require('graphql-fetch');
 const convictPromise = require('./config/convict.js');
 const introspectionQuery = require('./graphql/Tiamat/introspection').introspectionQuery;
 
-const fetchSchema = async () => {
-  const convict = await convictPromise;
+convictPromise.then(convict => {
   const url = convict.get('tiamatBaseUrl');
-  const response = await graphqlFetch(url)(introspectionQuery);
-
+  return graphqlFetch(url)(introspectionQuery);
+}).then(response => {
   fs.writeFileSync(
     './graphql/Tiamat/schema.json',
     JSON.stringify(response.data),
     'utf8'
   );
-}
-
-try {
-  fetchSchema();
-} catch(err) {
-  console.log("Unable to fetch schema, server exited with error");
+}).catch(err => {
+  console.log("Unable to fetch schema, server exited with error", err);
   process.exit(1);
-}
+});
