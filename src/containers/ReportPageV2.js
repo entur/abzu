17526c903d58731df9e6c00168e53ea2a75b9d0c
Tom Erik Støwer <testower@gmail.com>
2020-05-12 20:43:40 +0200

    ROR-804 Add tags, progress and pagination
    Tags and pagination from existing components.

--- src/containers/ReportPageV2.js
+++ src/containers/ReportPageV2.js
@@ -4,7 +4,14 @@ import React, { useState } from "react";
 import { connect } from "react-redux";
 import { withApollo } from "react-apollo";
 import { injectIntl } from "react-intl";
-import { Box, TextField, Button, Container } from "@material-ui/core";
+import {
+  Box,
+  TextField,
+  Button,
+  Container,
+  LinearProgress,
+} from "@material-ui/core";
+import { Alert } from "@material-ui/lab";
 import StopTypeFilter from "../components/ReportPageV2/StopTypeFilter";
 import {
   buildReportSearchQuery,
@@ -22,6 +29,8 @@ import {
   columnOptionsStopPlace,
 } from "../config/columnOptions";
 import ColumnFilterPopover from "../components/EditStopPage/ColumnFilterPopover";
+import TagFilterTray from "../components/ReportPage/TagFilterTray";
+import ReportPageFooter from "../components/ReportPage/ReportPageFooter";
 
 const updateColumnOption = (selected, options) => {
   return options.map((opt) => {
@@ -39,6 +48,16 @@ const updateAllOption = (checked, options) => {
   });
 };
 
+const handleTagItemCheck = ({ name, checked }, tags) => {
+  let nextTags = tags.slice();
+  if (checked) {
+    nextTags.push(name);
+  } else {
+    nextTags = nextTags.filter((tag) => tag !== name);
+  }
+  return nextTags;
+};
+
 const ReportPageV2 = ({ intl, client, results, duplicateInfo }) => {
   const { formatMessage } = intl;
   const [searchQuery, setSearchQuery] = useState("");
@@ -48,20 +67,24 @@ const ReportPageV2 = ({ intl, client, results, duplicateInfo }) => {
     columnOptionsStopPlace
   );
   const [quayColumns, setQuayColumns] = useState(columnOptionsQuays);
+  const [tags, setTags] = useState([]);
+  const [activePageIndex, setActivePageIndex] = useState(0);
 
   const topoiChips = [];
-  const activePageIndex = 0;
 
-  const isLoading = false;
+  const [isLoading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
+
   const withoutLocationOnly = false;
   const withDuplicateImportedIds = false;
   const withNearbySimilarDuplicates = false;
   const hasParking = false;
   const showFutureAndExpired = false;
   const withTags = false;
-  const tags = [];
 
   const handleSearch = async () => {
+    setLoading(true);
+
     const queryVariables = {
       query: searchQuery,
       withoutLocationOnly,
@@ -89,111 +112,128 @@ const ReportPageV2 = ({ intl, client, results, duplicateInfo }) => {
         .map((topos) => topos.id),
     };
 
-    const stopPlaceResponse = await client.query({
-      query: findStopForReport,
-      fetchPolicy: "network-only",
-      variables: queryVariables,
-    });
+    try {
+      const stopPlaceResponse = await client.query({
+        query: findStopForReport,
+        fetchPolicy: "network-only",
+        variables: queryVariables,
+      });
 
-    const stopPlaces = stopPlaceResponse.data.stopPlace;
-    const stopPlaceIds = [];
-    for (let i = 0; i < stopPlaces.length; i++) {
-      if (stopPlaces[i].__typename == "ParentStopPlace") {
-        const childStops = stopPlaces[i].children;
-        for (let j = 0; j < childStops.length; j++) {
-          stopPlaceIds.push(childStops[j].id);
+      const stopPlaces = stopPlaceResponse.data.stopPlace;
+      const stopPlaceIds = [];
+      for (let i = 0; i < stopPlaces.length; i++) {
+        if (stopPlaces[i].__typename == "ParentStopPlace") {
+          const childStops = stopPlaces[i].children;
+          for (let j = 0; j < childStops.length; j++) {
+            stopPlaceIds.push(childStops[j].id);
+          }
+        } else {
+          stopPlaceIds.push(stopPlaces[i].id);
         }
-      } else {
-        stopPlaceIds.push(stopPlaces[i].id);
       }
+
+      buildReportSearchQuery({
+        ...queryVariables,
+        showFutureAndExpired,
+      });
+
+      await client.query({
+        query: getParkingForMultipleStopPlaces(stopPlaceIds),
+        reducer: reportReducer,
+        fetchPolicy: "network-only",
+        operationName: "multipleParkingQuery",
+      });
+    } catch (e) {
+      setError(e);
+    } finally {
+      setLoading(false);
     }
-
-    buildReportSearchQuery({
-      ...queryVariables,
-      showFutureAndExpired,
-    });
-
-    const parkingResponse = await client.query({
-      query: getParkingForMultipleStopPlaces(stopPlaceIds),
-      reducer: reportReducer,
-      fetchPolicy: "network-only",
-      operationName: "multipleParkingQuery",
-    });
-
-    // this.setState({
-    //     isLoading: false,
-    //     activePageIndex: 0
-    // });
-
-    // .catch(err => {
-    // this.setState({
-    //     isLoading: false
-    // });
-    // });
   };
 
   return (
-    <Container>
-      <Box>
-        <TextField
-          label="Optional search query"
-          value={searchQuery}
-          onChange={(e) => setSearchQuery(e.target.value)}
-        />
-      </Box>
-      <Box>
-        <StopTypeFilter value={stopTypeFilter} onChange={setStopTypeFilter} />
-      </Box>
-      <Box>
-        <Button variant="outlined" onClick={handleSearch}>
-          Search
-        </Button>
-      </Box>
-      <Box>
-        <div style={{ display: "flex" }}>
-          <ColumnFilterPopover
-            style={{ marginLeft: 2, marginTop: 5, transform: "scale(0.9)" }}
-            columnOptions={stopPlaceColumns}
-            handleColumnCheck={(id, checked) =>
-              setStopPlaceColumns(
-                updateColumnOption({ id, checked }, stopPlaceColumns)
-              )
-            }
-            buttonLabel={formatMessage({
-              id: "column_filter_label_stop_place",
-            })}
-            captionLabel={formatMessage({ id: "stop_place" })}
+    <>
+      <Container>
+        <Box display="flex" alignItems="baseline">
+          <StopTypeFilter value={stopTypeFilter} onChange={setStopTypeFilter} />
+          <TagFilterTray
+            tags={tags}
             formatMessage={formatMessage}
-            handleCheckAll={(checked) =>
-              setStopPlaceColumns(updateAllOption(checked, stopPlaceColumns))
+            handleItemOnCheck={(name, checked) =>
+              setTags(handleTagItemCheck({ name, checked }, tags))
             }
-            selectAllLabel={formatMessage({ id: "all" })}
           />
-          <ColumnFilterPopover
-            style={{ marginLeft: 2, marginTop: 5, transform: "scale(0.9)" }}
-            columnOptions={quayColumns}
-            handleColumnCheck={(id, checked) =>
-              setQuayColumns(updateColumnOption({ id, checked }, quayColumns))
-            }
-            buttonLabel={formatMessage({ id: "column_filter_label_quays" })}
-            captionLabel={formatMessage({ id: "quays" })}
-            formatMessage={formatMessage}
-            handleCheckAll={(checked) =>
-              setQuayColumns(updateAllOption(checked, quayColumns))
-            }
-            selectAllLabel={formatMessage({ id: "all" })}
+        </Box>
+        <Box display="flex">
+          <TextField
+            label="Optional search query"
+            value={searchQuery}
+            onChange={(e) => setSearchQuery(e.target.value)}
           />
-        </div>
-        <ReportResultView
-          results={results}
-          intl={intl}
-          stopPlaceColumnOptions={stopPlaceColumns}
-          activePageIndex={activePageIndex}
-          quaysColumnOptions={quayColumns}
-          duplicateInfo={duplicateInfo}
-        />
-      </Box>
-    </Container>
+          <Button variant="outlined" onClick={handleSearch}>
+            Search
+          </Button>
+        </Box>
+        <Box>
+          <div style={{ display: "flex" }}>
+            <ColumnFilterPopover
+              style={{ marginLeft: 2, marginTop: 5, transform: "scale(0.9)" }}
+              columnOptions={stopPlaceColumns}
+              handleColumnCheck={(id, checked) =>
+                setStopPlaceColumns(
+                  updateColumnOption({ id, checked }, stopPlaceColumns)
+                )
+              }
+              buttonLabel={formatMessage({
+                id: "column_filter_label_stop_place",
+              })}
+              captionLabel={formatMessage({ id: "stop_place" })}
+              formatMessage={formatMessage}
+              handleCheckAll={(checked) =>
+                setStopPlaceColumns(updateAllOption(checked, stopPlaceColumns))
+              }
+              selectAllLabel={formatMessage({ id: "all" })}
+            />
+            <ColumnFilterPopover
+              style={{ marginLeft: 2, marginTop: 5, transform: "scale(0.9)" }}
+              columnOptions={quayColumns}
+              handleColumnCheck={(id, checked) =>
+                setQuayColumns(updateColumnOption({ id, checked }, quayColumns))
+              }
+              buttonLabel={formatMessage({ id: "column_filter_label_quays" })}
+              captionLabel={formatMessage({ id: "quays" })}
+              formatMessage={formatMessage}
+              handleCheckAll={(checked) =>
+                setQuayColumns(updateAllOption(checked, quayColumns))
+              }
+              selectAllLabel={formatMessage({ id: "all" })}
+            />
+          </div>
+          {isLoading ? (
+            <LinearProgress />
+          ) : (
+            !error && (
+              <ReportResultView
+                results={results}
+                intl={intl}
+                stopPlaceColumnOptions={stopPlaceColumns}
+                activePageIndex={activePageIndex}
+                quaysColumnOptions={quayColumns}
+                duplicateInfo={duplicateInfo}
+              />
+            )
+          )}
+          {error && <Alert severity="error">{error.message}</Alert>}
+        </Box>
+      </Container>
+      <ReportPageFooter
+        results={results}
+        intl={intl}
+        stopPlaceColumnOptions={stopPlaceColumns}
+        quaysColumnOptions={quayColumns}
+        handleSelectPage={setActivePageIndex}
+        activePageIndex={activePageIndex}
+      />
+    </>
   );
 };
 
