29ea3649eef3a92db77e43af5afc047d31d9821b
Tom Erik Støwer <testower@gmail.com>
2021-09-08 20:43:41 +0200

    Boarding position marker

---
+++ src/components/Map/BoardingPositionMarker.tsx
@@ -0,0 +1,26 @@
+import React from "react";
+import ReactDOM from "react-dom/server";
+import { Marker, Popup } from "react-leaflet";
+import { divIcon } from "leaflet";
+import BoardingPositionMarkerIcon from "./BoardingPositionMarkerIcon";
+import { useMemo } from "react";
+
+type Props = {
+  position: [number, number];
+  publicCode: string;
+};
+
+export default ({ position, publicCode }: Props) => {
+  const divBody = ReactDOM.renderToStaticMarkup(
+    <BoardingPositionMarkerIcon publicCode={publicCode} />
+  );
+
+  let icon = divIcon({
+    html: divBody,
+    iconSize: [22, 34],
+    iconAnchor: [11, 34],
+    popupAnchor: [5, 0],
+  });
+
+  return <Marker draggable={true} position={position} icon={icon} />;
+};
