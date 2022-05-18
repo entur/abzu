818af3840c1f736ce10e6a9296db6901a042f9be
Tom Erik Støwer <testower@gmail.com>
2022-05-18 21:48:15 +0200

    Memoize url and params

--- src/components/Map/WMTSLayer.tsx
+++ src/components/Map/WMTSLayer.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useMemo } from "react";
 import { TileLayer } from "react-leaflet";
 import L from "leaflet";
 
@@ -8,22 +8,31 @@ export interface WMTSLayerProps {
 }
 
 export const WMTSLayer: React.FC<WMTSLayerProps> = ({ baseUrl, token }) => {
-  const wmtsParams = L.Util.getParamString({
-    service: "WMTS",
-    request: "GetTile",
-    version: "1.1.1",
-    style: "default",
-    format: "image/png",
-    transparent: "false",
-    tilematrixSet: "default028mm",
-    layers: "toporaster2",
-    gkt: token,
-  });
+  const wmtsParams = useMemo(
+    () =>
+      L.Util.getParamString({
+        service: "WMTS",
+        request: "GetTile",
+        version: "1.1.1",
+        style: "default",
+        format: "image/png",
+        transparent: "false",
+        tilematrixSet: "default028mm",
+        layers: "toporaster2",
+        gkt: token,
+      }),
+    [token]
+  );
+
+  const url = useMemo(
+    () => `${baseUrl}${wmtsParams}&tilematrix={z}&tilerow={y}&tilecol={x}`,
+    [baseUrl, wmtsParams]
+  );
 
   return (
     <TileLayer
       attribution='&copy; <a href="http://www.kartverket.no">Kartverket'
-      url={`${baseUrl}${wmtsParams}&tilematrix={z}&tilerow={y}&tilecol={x}`}
+      url={url}
       maxZoom={19}
     />
   );
