a187bcb0fcec0582b3b729603748254b5efa8158
Tom Erik Støwer <testower@gmail.com>
2022-05-23 16:04:12 +0200

    Fix rebase

--- src/react-app-env.d.ts
+++ src/react-app-env.d.ts
@@ -1,15 +1,5 @@
 /// <reference types="react-scripts" />
 
-export {};
-
 declare module "redux-raven-middleware";
 declare module "material-ui/TextField";
 declare module "material-ui/svg-icons/action/delete-forever";
-
-declare global {
-  interface Window {
-    config: {
-      endpointBase: string;
-    };
-  }
-}
