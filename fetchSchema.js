9c0addb7ecb01e4292b316ed8327f10d266bd5d7
Tom Erik Støwer <testower@gmail.com>
2022-05-26 11:11:26 +0200

    Replace graphql-fetch with graphql-request due to security alert

--- fetchSchema.js
+++ fetchSchema.js
@@ -1,17 +1,30 @@
 #!/usr/bin/env node
 
 var fs = require('fs');
-const graphqlFetch = require('graphql-fetch');
+const { request, gql } = require('graphql-request');
 const convictPromise = require('./src/config/convict.js');
-const introspectionQuery = require('./src/graphql/Tiamat/introspection').introspectionQuery;
+
+const introspectionQuery = gql`
+{
+  __schema {
+    types {
+      kind
+      name
+      possibleTypes {
+        name
+      }
+    }
+  }
+}
+`;
 
 convictPromise.then(convict => {
   const url = convict.get('tiamatBaseUrl');
-  return graphqlFetch(url)(introspectionQuery);
+  return request(url, introspectionQuery);
 }).then(response => {
   fs.writeFileSync(
     './src/graphql/Tiamat/schema.json',
-    JSON.stringify(response.data),
+    JSON.stringify(response),
     'utf8'
   );
 }).catch(err => {
