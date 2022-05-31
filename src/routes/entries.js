1abf39321656a17c5b724a8e55ba8f6a229a7ab5
Tom Erik Støwer <testower@gmail.com>
2022-05-31 10:07:12 +0200

    Remove file that is no longer in use

--- src/routes/entries.js
+++
@@ -1,9 +0,0 @@
-const Routes = require("./index");
-
-const getRouteEntries = (endPointBase, path) => {
-  return Object.keys(Routes).map((key) => endPointBase + Routes[key] + path);
-};
-
-module.exports = {
-  getRouteEntries: getRouteEntries,
-};
