542a1d5b685490d39fe02c6084a3de9fa4ff45a9
Tom Erik Støwer <testower@gmail.com>
2022-06-30 14:13:20 +0200

    WTK-1452 Update query for quay usage

--- src/utils/shamash.js
+++ src/utils/shamash.js
@@ -7,7 +7,7 @@ const getShamashBaseUrl = () =>
 export const getQuaySearchUrl = (id) => {
   const encodedId = encodeURI(id);
 
-  const query = `%7Bquay%28id%3A%22${encodedId}%22%29%7Bname%20id%20lines%7Bid%20publicCode%20serviceJourneys%7Bid%20activeDates%7D%7D%7D%7D&variables=`;
+  const query = `%7Bquay%28id%3A%22${encodedId}%22%29%7Bname%20id%20publicCode%20journeyPatterns%7BserviceJourneys%7Bid%20privateCode%20line%7Bid%20publicCode%7DtransportMode%20activeDates%7D%7D%7D%7D&variables=`;
   return `${getShamashBaseUrl()}?query=${query}`;
 };
 
