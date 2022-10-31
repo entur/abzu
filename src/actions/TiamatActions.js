f2dad7523401fc62fae36648c2036b6cdc1207d1
Tom Erik Støwer <testower@gmail.com>
2022-10-31 12:26:43 +0100

    fix: using await inside string template literal does not work as expected

--- src/actions/TiamatActions.js
+++ src/actions/TiamatActions.js
@@ -63,6 +63,8 @@ import { v4 as uuidv4 } from "uuid";
 import { getTiamatClient } from "../graphql/clients";
 
 export const getContext = async (auth) => {
+  const token = await auth.getAccessToken();
+
   const context = {
     headers: {
       "X-Correlation-Id": uuidv4(),
@@ -70,7 +72,6 @@ export const getContext = async (auth) => {
   };
 
   if (auth.isAuthenticated) {
-    const token = await auth.getAccessToken();
     context.headers["Authorization"] = `Bearer ${token}`;
   }
 
