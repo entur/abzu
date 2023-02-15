b979362d75811d6cdd52596b9db03eadf0ae7a62
Mansoor Sajjad <mansoor.sajjad@entur.org>
2023-02-15 13:59:00 +0100

    Adding new firebase site for abzu nordic

--- src/config/getEnvironment.ts
+++ src/config/getEnvironment.ts
@@ -3,6 +3,8 @@ export const getEnvironment = () => {
     return "prod";
   } else if (window.location.hostname === "stoppested.staging.entur.org") {
     return "test";
+  } else if (window.location.hostname === "stoppested-nordic.dev.entur.org") {
+    return "nordic-dev";
   } else {
     return "dev";
   }
