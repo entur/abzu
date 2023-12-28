7e1e92ef161518812cda7cbe0bdf149ffe2f7421
Tom Erik Støwer <testower@gmail.com>
2023-12-28 13:29:52 +0100

    Formatting

--- src/auth/auth.test.tsx
+++ src/auth/auth.test.tsx
@@ -1,80 +1,80 @@
-import { renderHook } from '@testing-library/react';
+import { renderHook } from "@testing-library/react";
 
-import {useAuth} from './auth';
-import { ConfigContext } from '../config/ConfigContext';
+import { useAuth } from "./auth";
+import { ConfigContext } from "../config/ConfigContext";
 
-jest.mock('react-oidc-context', () => {
-    return {
-        ...jest.requireActual('react-oidc-context'),
-        useAuth: () => ({
-            isLoading: false,
-            isAuthenticated: true,
-            activeNavigator: 'signinRedirect',
-            user: {
-                profile: {
-                    preferred_name: 'Test Name',
-                    claims: ['{"r":"adminEditRouteData","o":"RB"}'],
-                },
-                access_token: 'test-token',
-            },
-            signoutRedirect: () => Promise.resolve(),
-            signinRedirect: () => Promise.resolve(),
-        }),
-    };
+jest.mock("react-oidc-context", () => {
+  return {
+    ...jest.requireActual("react-oidc-context"),
+    useAuth: () => ({
+      isLoading: false,
+      isAuthenticated: true,
+      activeNavigator: "signinRedirect",
+      user: {
+        profile: {
+          preferred_name: "Test Name",
+          claims: ['{"r":"adminEditRouteData","o":"RB"}'],
+        },
+        access_token: "test-token",
+      },
+      signoutRedirect: () => Promise.resolve(),
+      signinRedirect: () => Promise.resolve(),
+    }),
+  };
 });
 
 const wrapper = ({ children }: { children: React.ReactNode }) => (
-    <ConfigContext.Provider
-        value={{
-            preferredNameNamespace: 'preferred_name',
-            claimsNamespace: 'claims',
-        }}
-    >
-        {children}
-    </ConfigContext.Provider>
+  <ConfigContext.Provider
+    value={{
+      preferredNameNamespace: "preferred_name",
+      claimsNamespace: "claims",
+    }}
+  >
+    {children}
+  </ConfigContext.Provider>
 );
 
 const testHook = () => {
-    return renderHook(() => useAuth(), { wrapper });
+  return renderHook(() => useAuth(), { wrapper });
 };
 
-describe('useAuth', () => {
-    test('isLoading', () => {
-        const { result } = testHook();
-        expect(result.current.isLoading).toBe(false);
-    });
+describe("useAuth", () => {
+  test("isLoading", () => {
+    const { result } = testHook();
+    expect(result.current.isLoading).toBe(false);
+  });
 
-    test('isAuthenticated', () => {
-        const { result } = testHook();
-        expect(result.current.isAuthenticated).toBe(true);
-    });
+  test("isAuthenticated", () => {
+    const { result } = testHook();
+    expect(result.current.isAuthenticated).toBe(true);
+  });
 
-    test('user.name', () => {
-        const { result } = testHook();
-        expect(result.current.user?.name).toBe('Test Name');
-    });
+  test("user.name", () => {
+    const { result } = testHook();
+    expect(result.current.user?.name).toBe("Test Name");
+  });
 
-    test('roleAssignments', () => {
-        const { result } = testHook();
-        expect(result.current.roleAssignments).toEqual([
-            '{"r":"adminEditRouteData","o":"RB"}',
-        ]);
-    });
+  test("roleAssignments", () => {
+    const { result } = testHook();
+    expect(result.current.roleAssignments).toEqual([
+      '{"r":"adminEditRouteData","o":"RB"}',
+    ]);
+  });
 
-    test('getAccessToken', async () => {
-        const { result } = testHook();
-        expect(await result.current.getAccessToken()).toBe('test-token');
-    });
+  test("getAccessToken", async () => {
+    const { result } = testHook();
+    expect(await result.current.getAccessToken()).toBe("test-token");
+  });
 
-    test('login', async () => {
-        const { result } = testHook();
-        await expect(result.current.login()).resolves.not.toThrowError();
-    });
+  test("login", async () => {
+    const { result } = testHook();
+    await expect(result.current.login()).resolves.not.toThrowError();
+  });
 
-    test('logout', async () => {
-        const { result } = testHook();
-        await expect(
-            result.current.logout({ returnTo: '/' })
-        ).resolves.not.toThrowError();
-    });
+  test("logout", async () => {
+    const { result } = testHook();
+    await expect(
+      result.current.logout({ returnTo: "/" }),
+    ).resolves.not.toThrowError();
+  });
 });
