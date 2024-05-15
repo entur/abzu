d6c54ee4936b535b861397b4ffdf679c18aaca41
a-limyr <andreas.limyr@entur.org>
2024-05-15 12:46:32 +0200

    Missing coordinates when saving - possible fix.

--- src/graphql/Tiamat/mutations.js
+++ src/graphql/Tiamat/mutations.js
@@ -139,7 +139,7 @@ export const mutateStopPlace = gql`
     $publicCode: String
     $privateCode: PrivateCodeInput
     $description: EmbeddableMultilingualStringInput
-    $legacyCoordinates: legacyCoordinates
+    $coordinates: legacyCoordinates
     $stopPlaceType: StopPlaceType
     $quays: [QuayInput]
     $validBetween: ValidBetweenInput
