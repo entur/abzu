cc89b3c400cc14d340ace2e29852554ef9ee7094
a-limyr <andreas.limyr@entur.org>
2025-09-18 10:03:33 +0200

    Saves location before login and restores it after logging in is complete.

--- src/containers/App.js
+++ src/containers/App.js
@@ -59,6 +59,13 @@ const App = ({ children }) => {
     dispatch(updateAuth(auth));
     if (!auth.isLoading) {
       dispatch(fetchUserPermissions());
+      if (auth.isAuthenticated) {
+        const redirectPath = sessionStorage.getItem("redirectAfterLogin");
+        if (redirectPath) {
+          sessionStorage.removeItem("redirectAfterLogin");
+          dispatch(UserActions.navigateTo(redirectPath.replace("/", ""), ""));
+        }
+      }
     }
   }, [auth]);
 
