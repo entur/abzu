3d90c07a496c0c2415f9631e9a20b38ffa49f066
a-limyr <andreas.limyr@entur.org>
2026-02-13 14:53:08 +0100

    Made Kartverket flyfoto layer fetch zoom values from config.

--- src/ext/KartverketFlyFoto/KartverketFlyFotoLayer.tsx
+++ src/ext/KartverketFlyFoto/KartverketFlyFotoLayer.tsx
@@ -1,15 +1,13 @@
 import { FeatureComponent } from "@entur/react-component-toggle";
 import { useMemo } from "react";
 import { WMTSLayer } from "../../components/Map/WMTSLayer";
+import { Tile } from "../../config/ConfigContext";
 import { useGktToken } from "./hooks/useGktToken";
-import { KartverketFlyFotoLayerProps } from "./types";
 
 const BASE_URL =
   "https://gatekeeper1.geonorge.no/BaatGatekeeper/gk/gk.nib_web_mercator_wmts_v2";
 
-export const KartverketFlyFotoLayer: FeatureComponent<
-  KartverketFlyFotoLayerProps
-> = () => {
+export const KartverketFlyFotoLayer: FeatureComponent<Tile> = (props) => {
   const token = useGktToken();
 
   const params = useMemo(() => {
@@ -27,6 +25,8 @@ export const KartverketFlyFotoLayer: FeatureComponent<
       baseUrl={BASE_URL}
       params={params}
       attribution='&copy; <a href="http://www.kartverket.no">Kartverket'
+      maxZoom={props.maxZoom}
+      maxNativeZoom={props.maxNativeZoom}
     />
   );
 };
