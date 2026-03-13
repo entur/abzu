eade2480199588f189d55a7441b67eadfc396e2d
Topi Penttinen <212693087+solita-topip@users.noreply.github.com>
2026-03-13 09:43:10 +0200

    Restructure dispatch order

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -51,13 +51,13 @@ class Header extends React.Component {
   }
 
   goToMain() {
+    this.props.dispatch(UserActions.navigateTo("/", ""));
+    this.props.dispatch(UserActions.toggleShowFutureAndExpired(false));
+    this.props.dispatch(UserActions.setShowMoreFilterOptions(false));
     this.props.dispatch(UserActions.clearSearchResults());
     this.props.dispatch(UserActions.clearActiveSearchResults());
     this.props.dispatch(UserActions.setSearchText(""));
     this.props.dispatch(UserActions.removeAllFilters());
-    this.props.dispatch(UserActions.navigateTo("/", ""));
-    this.props.dispatch(UserActions.toggleShowFutureAndExpired(false));
-    this.props.dispatch(UserActions.setShowMoreFilterOptions(false));
   }
 
   handleConfirmChangeRoute(next, action) {
