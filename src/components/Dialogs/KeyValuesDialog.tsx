2b6b69e08ccdc242767f68a1332192c6f2d4a7b2
Sabina Fataliyeva <sabina.fataliyeva@solita.fi>
2026-04-27 17:55:00 +0300

    Shift the contents of KeyValuesDialog into TypeScript

--- src/components/Dialogs/KeyValuesDialog.tsx
+++ src/components/Dialogs/KeyValuesDialog.tsx
@@ -16,161 +16,221 @@ import Add from "@mui/icons-material/Add";
 import MdRemove from "@mui/icons-material/Delete";
 import MdEdit from "@mui/icons-material/ModeEdit";
 import Fab from "@mui/material/Fab";
-import React from "react";
-import { connect } from "react-redux";
+import { UnknownAction } from "@reduxjs/toolkit";
+import React, { useState } from "react";
+import { useIntl } from "react-intl";
+import { useDispatch, useSelector } from "react-redux";
 import { StopPlaceActions, UserActions } from "../../actions/";
+import { useConfig } from "../../config/ConfigContext";
 import { getPrimaryColor } from "../../config/themeConfig";
+import { KeyValues } from "../../models/KeyValues";
 import { selectKeyValuesDataSource } from "../../reducers/selectors";
 import CreateKeyValuePair from "../EditStopPage/CreateKeyValuePair";
 import EditKeyValuePair from "../EditStopPage/EditKeyValuePair";
 import DialogHeader from "./DialogHeader";
 
-class KeyValuesDialog extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      isEditingOpen: false,
-      isCreatingOpen: false,
-      editingKey: null,
-    };
-  }
+type KeyValuesDialogProps = {
+  disabled: boolean;
+};
 
-  handleEditValuesForKey(key) {
-    this.setState({
-      isEditingOpen: true,
-      isCreatingOpen: false,
-      editingKey: key,
-    });
-  }
+const KeyValuesDialog = ({ disabled }: KeyValuesDialogProps) => {
+  const dispatch = useDispatch();
+  const { formatMessage } = useIntl();
+  const open = useSelector((state: any) => state.user.keyValuesDialogOpen);
+  const keyValues: KeyValues[] = useSelector((state: any) =>
+    selectKeyValuesDataSource(
+      state.user.keyValuesOrigin,
+      state.stopPlace.current,
+    ),
+  );
+  const keyValuesOrigin = useSelector(
+    (state: any) => state.user.keyValuesOrigin?.type,
+  );
+  disabled = false;
 
-  handleDeleteKey(key) {
-    this.props.dispatch(StopPlaceActions.deleteKeyValuesByKey(key));
-  }
+  const { mandatoryKeyValuesSet } = useConfig();
+  const mandatoryKeys =
+    keyValuesOrigin && mandatoryKeyValuesSet
+      ? keyValuesOrigin == "stopPlace"
+        ? mandatoryKeyValuesSet.stopPlace
+        : mandatoryKeyValuesSet.quay
+      : [];
 
-  handleUpdateValues(key, values) {
-    this.setState({
-      isEditingOpen: false,
-    });
-    this.props.dispatch(StopPlaceActions.updateKeyValuesForKey(key, values));
-  }
+  // Because some of the mandatory keys may already be in use in keyValues and have a value there:
+  const mandatoryKeyValues: KeyValues[] = mandatoryKeys
+    ? mandatoryKeys.map((mandatoryKey) => {
+        const existingKeyValues = keyValues.find(
+          (kv) => kv.key === mandatoryKey,
+        );
+        if (!existingKeyValues) {
+          return {
+            key: mandatoryKey,
+            values: null,
+          };
+        } else {
+          return existingKeyValues;
+        }
+      })
+    : [];
+  const nonMandatoryKeyValues = mandatoryKeys
+    ? keyValues?.filter((kv) => !mandatoryKeys.find((mk) => mk === kv.key))
+    : keyValues;
 
-  handleCreateValues(key, values) {
-    this.setState({
-      isCreatingOpen: false,
-    });
-    this.props.dispatch(StopPlaceActions.createKeyValuesPair(key, values));
-  }
+  // This is meant to group mandatory keys together and show them in the beginning of the key values list:
+  const keyValuesCompleteSet: KeyValues[] = [
+    ...mandatoryKeyValues,
+    ...nonMandatoryKeyValues,
+  ];
 
-  handleOpenCreateValues() {
-    this.setState({
-      isEditingOpen: false,
-      isCreatingOpen: true,
-    });
-  }
+  const [isEditingOpen, setIsEditingOpen] = useState<boolean>(false);
+  const [isCreatingOpen, setIsCreatingOpen] = useState<boolean>(false);
+  const [editingKey, setEditingKey] = useState<string | null>(null);
 
-  handleClose() {
-    this.props.dispatch(UserActions.closeKeyValuesDialog());
-    this.setState({
-      isEditingOpen: false,
-      isCreatingOpen: false,
-    });
-  }
+  const translations = {
+    value: formatMessage({ id: "name" }),
+    keyValuesTitle: formatMessage({ id: "key_values_hint" }),
+    noKeyValues: formatMessage({ id: "key_values_no" }),
+  };
 
-  render() {
-    const { open, intl, keyValues, disabled } = this.props;
-    const { formatMessage } = intl;
+  const handleEditValuesForKey = (key: string) => {
+    setIsEditingOpen(true);
+    setIsCreatingOpen(false);
+    setEditingKey(key);
+  };
 
-    const translations = {
-      value: formatMessage({ id: "name" }),
-      keyValuesTitle: formatMessage({ id: "key_values_hint" }),
-      noKeyValues: formatMessage({ id: "key_values_no" }),
-    };
+  const handleDeleteKey = (key: string) => {
+    dispatch(
+      StopPlaceActions.deleteKeyValuesByKey(key) as unknown as UnknownAction,
+    );
+  };
 
-    if (!open) return null;
+  const handleUpdateValues = (key: string, values: string[]) => {
+    setIsEditingOpen(false);
 
-    const style = {
-      position: "fixed",
-      left: 400,
-      top: 105,
-      background: "#fff",
-      border: "1px solid black",
-      width: "auto",
-      minWidth: 350,
-      zIndex: 999,
-    };
+    if (!keyValues.find((kv) => kv.key === key)) {
+      dispatch(
+        StopPlaceActions.createKeyValuesPair(
+          key,
+          values,
+        ) as unknown as UnknownAction,
+      );
+    }
 
-    const itemStyle = {
-      flexBasis: "100%",
-      textAlign: "left",
-      flex: 2,
-      padding: 5,
-    };
+    dispatch(
+      StopPlaceActions.updateKeyValuesForKey(
+        key,
+        values,
+      ) as unknown as UnknownAction,
+    );
+  };
 
-    return (
-      <div style={style}>
-        <DialogHeader
-          title={translations.keyValuesTitle}
-          handleClose={this.handleClose.bind(this)}
-        />
-        <div
-          style={{
-            fontSize: 14,
-            maxHeight: 300,
-            overflowY: "auto",
-            marginLeft: 15,
-            marginBottom: 5,
-          }}
-        >
-          {!keyValues.length ? (
-            <div
-              style={{
-                width: "100%",
-                textAlign: "center",
-                marginBottom: 10,
-                fontSize: 12,
-              }}
-            >
-              {translations.noKeyValues}
-            </div>
-          ) : (
-            <div
-              style={{
-                fontSize: 12,
-                overflowY: "overlay",
-                marginLeft: 5,
-              }}
-            >
-              {keyValues.map((kvp, i) => (
-                <div
-                  key={"key-value-" + i}
-                  style={{
-                    display: "flex",
-                    justifyContent: "space-between",
-                    lineHeight: 1.2,
-                  }}
-                >
-                  <div style={{ ...itemStyle, fontWeight: 600, flex: 1 }}>
-                    <div
-                      style={{
-                        display: "flex",
-                        alignItems: "center",
-                        flexBasis: "100%",
-                      }}
-                    >
-                      <span>{kvp.key}</span>
-                      {!disabled && (
-                        <div style={{ display: "flex" }}>
-                          <MdEdit
-                            style={{
-                              height: 16,
-                              width: 16,
-                              color: getPrimaryColor(),
-                              marginTop: -2,
-                              marginLeft: 5,
-                              cursor: "pointer",
-                            }}
-                            onClick={() => this.handleEditValuesForKey(kvp.key)}
-                          />
+  const handleCreateValues = (key: string, values: string[]) => {
+    setIsCreatingOpen(false);
+    dispatch(
+      StopPlaceActions.createKeyValuesPair(
+        key,
+        values,
+      ) as unknown as UnknownAction,
+    );
+  };
+
+  const handleOpenCreateValues = () => {
+    setIsEditingOpen(false);
+    setIsCreatingOpen(true);
+  };
+
+  const handleClose = () => {
+    dispatch(UserActions.closeKeyValuesDialog() as unknown as UnknownAction);
+    setIsEditingOpen(false);
+    setIsCreatingOpen(false);
+  };
+
+  const style: React.CSSProperties = {
+    position: "fixed",
+    left: 400,
+    top: 105,
+    background: "#fff",
+    border: "1px solid black",
+    width: "auto",
+    minWidth: 350,
+    zIndex: 999,
+  };
+
+  const itemStyle: React.CSSProperties = {
+    flexBasis: "100%",
+    textAlign: "left",
+    flex: 2,
+    padding: 5,
+  };
+
+  if (!open) return null;
+
+  return (
+    <div style={style}>
+      <DialogHeader
+        title={translations.keyValuesTitle}
+        handleClose={handleClose}
+      />
+      <div
+        style={{
+          fontSize: 14,
+          maxHeight: 300,
+          overflowY: "auto",
+          marginLeft: 15,
+          marginBottom: 5,
+        }}
+      >
+        {!keyValuesCompleteSet.length ? (
+          <div
+            style={{
+              width: "100%",
+              textAlign: "center",
+              marginBottom: 10,
+              fontSize: 12,
+            }}
+          >
+            {translations.noKeyValues}
+          </div>
+        ) : (
+          <div
+            style={{
+              fontSize: 12,
+              overflowY: "overlay",
+              marginLeft: 5,
+            }}
+          >
+            {keyValuesCompleteSet.map((kvp, i) => (
+              <div
+                key={"key-value-" + i}
+                style={{
+                  display: "flex",
+                  justifyContent: "space-between",
+                  lineHeight: 1.2,
+                }}
+              >
+                <div style={{ ...itemStyle, fontWeight: 600, flex: 1 }}>
+                  <div
+                    style={{
+                      display: "flex",
+                      alignItems: "center",
+                      flexBasis: "100%",
+                    }}
+                  >
+                    <span>{kvp.key}</span>
+                    {!disabled && (
+                      <div style={{ display: "flex" }}>
+                        <MdEdit
+                          style={{
+                            height: 16,
+                            width: 16,
+                            color: getPrimaryColor(),
+                            marginTop: -2,
+                            marginLeft: 5,
+                            cursor: "pointer",
+                          }}
+                          onClick={() => handleEditValuesForKey(kvp.key)}
+                        />
+                        {!mandatoryKeys?.includes(kvp.key) && (
                           <MdRemove
                             style={{
                               height: 16,
@@ -180,59 +240,50 @@ class KeyValuesDialog extends React.Component {
                               marginLeft: 5,
                               cursor: "pointer",
                             }}
-                            onClick={() => this.handleDeleteKey(kvp.key)}
+                            onClick={() => handleDeleteKey(kvp.key)}
                           />
-                        </div>
-                      )}
-                    </div>
-                  </div>
-                  <div style={itemStyle}>
-                    {kvp.values.map((v, i) => (
-                      <p key={"value-" + i}>{v}</p>
-                    ))}
+                        )}
+                      </div>
+                    )}
                   </div>
                 </div>
-              ))}
-            </div>
-          )}
-        </div>
-        {!disabled && (
-          <Fab
-            onClick={this.handleOpenCreateValues.bind(this)}
-            //mini={true}
-            style={{
-              marginLeft: 20,
-              marginBottom: 10,
-              height: 10,
-              width: 40,
-              backgroundColor: getPrimaryColor(),
-            }}
-          >
-            <Add />
-          </Fab>
+                <div style={itemStyle}>
+                  {kvp.values?.map((v, i) => (
+                    <p key={"value-" + i}>{v}</p>
+                  ))}
+                </div>
+              </div>
+            ))}
+          </div>
         )}
-        <EditKeyValuePair
-          isOpen={this.state.isEditingOpen}
-          editingKey={this.state.editingKey}
-          keyValues={keyValues}
-          handleUpdateValues={this.handleUpdateValues.bind(this)}
-        />
-        <CreateKeyValuePair
-          isOpen={this.state.isCreatingOpen}
-          keyValues={keyValues}
-          handleCreateValues={this.handleCreateValues.bind(this)}
-        />
       </div>
-    );
-  }
-}
+      {!disabled && (
+        <Fab
+          onClick={handleOpenCreateValues}
+          style={{
+            marginLeft: 20,
+            marginBottom: 10,
+            height: 10,
+            width: 40,
+            backgroundColor: getPrimaryColor(),
+          }}
+        >
+          <Add />
+        </Fab>
+      )}
+      <EditKeyValuePair
+        isOpen={isEditingOpen}
+        editingKey={editingKey}
+        keyValues={keyValues}
+        handleUpdateValues={handleUpdateValues}
+      />
+      <CreateKeyValuePair
+        isOpen={isCreatingOpen}
+        keyValues={keyValues}
+        handleCreateValues={handleCreateValues}
+      />
+    </div>
+  );
+};
 
-const mapStateToProps = (state) => ({
-  open: state.user.keyValuesDialogOpen,
-  keyValues: selectKeyValuesDataSource(
-    state.user.keyValuesOrigin,
-    state.stopPlace.current,
-  ),
-});
-
-export default connect(mapStateToProps)(KeyValuesDialog);
+export default KeyValuesDialog;
