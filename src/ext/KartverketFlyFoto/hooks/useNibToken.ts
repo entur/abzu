12d335287fbe2b6db658c9c88539a526137fd6dd
Tom Erik Støwer <testower@gmail.com>
2026-05-04 18:35:07 +0200

    apply suggestions from review

--- src/ext/KartverketFlyFoto/hooks/useNibToken.ts
+++ src/ext/KartverketFlyFoto/hooks/useNibToken.ts
@@ -1,24 +1,25 @@
 import Axios from "axios";
-import { useContext, useEffect, useState } from "react";
+import { useContext, useEffect, useRef, useState } from "react";
 import { ConfigContext } from "../../../config/ConfigContext";
 
 const INTERVAL_IN_MS = 60000;
+const REFRESH_MARGIN_MS = 30 * 60 * 1000;
 
 export const useNibToken = () => {
   const [token, setToken] = useState<string | null>(null);
-  const [expires, setExpires] = useState(0);
+  const expiresRef = useRef(0);
 
   const config = useContext(ConfigContext);
 
   useEffect(() => {
     const fetchToken = async () => {
-      if (expires > Date.now() + 30 * 60 * 1000) {
+      if (expiresRef.current > Date.now() + REFRESH_MARGIN_MS) {
         return;
       }
       try {
         const response = await Axios.get(config.baatTokenProxyEndpoint!);
         setToken(response.data.token);
-        setExpires(response.data.expires);
+        expiresRef.current = response.data.expires;
       } catch (err) {
         console.warn(
           "Failed to get NIB token, ortofoto layer will not work",
